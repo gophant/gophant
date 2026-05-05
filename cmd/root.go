9bb81db9a9810a07e73df003453467a4281c0ed2
Kawser <kawser.ahmed@technonext.com>
2026-05-05 10:36:28 +0600

    updated with few chagnes

--- cmd/root.go
+++ cmd/root.go
@@ -35,3 +35,5 @@ func init() {
 	rootCmd.PersistentFlags().BoolVarP(&yes, "yes", "y", false, "Answer yes to prompts (non-interactive)")
 	// Subcommands are added in separate files
 }
+
+// git push origin main && git tag v0.1.1 && git push origin v0.1.1 && go clean -modcache && go install github.com/gophant/gophant@latest
